9a609919e6ab769c94bc7e47eba8f12df2f44d7b
Jacob M. Slone <jslone1993@gmail.com>
2014-11-19 17:29:50 -0500

    adding basic combat manager

---
+++ Feuds/Assets/Scripts/CombatController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct Damage {
+	public Damage(float physical, float magic) {
+		this.physical = physical;
+		this.magic = magic;
+	}
+
+	public float physical;
+	public float magic;
+	public float total { get { return Mathf.Max(physical + magic,0.0f); } }
+
+	public static Damage operator-(Damage atk, Damage def) {
+		return new Damage (Mathf.Max (atk.physical - def.physical, 0.0f), Mathf.Max (atk.magic - def.magic, 0.0f));
+	}
+
+	public static Damage operator*(float scalar, Damage atk) {
+		return new Damage (scalar * atk.physical, scalar * atk.magic);
+	}
+
+};
+
+public class CombatController : MonoBehaviour {
+	public float Health;
+	public float Speed;
+	public Damage Attack;
+	public Damage Defense;
+
+	public bool isDead { get {return Health <= 0;} }
+	public bool inCombat;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void DoDamage(CombatController other) {
+		other.TakeDamage (Random.Range(1.0f, 1.5f) * Attack);
+	}
+	
+	void TakeDamage(Damage atk) {
+		Health -= (atk - Random.Range (1.0f, 1.5f) * Defense).total * Random.Range (0.0f, 1.0f);
+	}
+
+	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info) {
+		stream.Serialize (ref Health);
+	}
+}
