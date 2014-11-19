64829bc98cc7738a662ba7a5c5fb77c111c0f666
paulaluri <paulaluri@Pauls-MBP.wv.cc.cmu.edu>
2014-11-19 16:49:15 -0500

    Initial inputmanager

---
+++ Feuds/Assets/Scripts/InputManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InputManager : MonoBehaviour {
+	public GameManager gameManager;
+	public List<GameObject> selectedCharacters;
+
+	//Selected characters attack
+	public void Attack(GameObject target){
+		foreach (GameObject g in selectedCharacters) {
+			g.GetComponent<ActionController>().Do(new Attack(target));
+		}
+	}
+
+	//Selected characters move to position
+	public void MoveTo(Vector3 target){
+		foreach (GameObject g in selectedCharacters) {
+			g.GetComponent<ActionController>().Do(new MoveTo(target));
+		}
+	}
+	
+	public void SelectCharacters(List<GameObject> characters){
+		selectedCharacters = characters;
+	}
+}
