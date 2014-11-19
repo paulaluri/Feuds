d4ace4643ae3a9629725167c1bce8a97d0d87d49
paulaluri <paulaluri@Pauls-MBP.wv.cc.cmu.edu>
2014-11-19 16:08:42 -0500

    New assets

---
+++ Feuds/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+	public static List<GameObject> characters;
+	public Vector3 mouseStartPosition;
+	public bool mouseDown;
+	public Rect selectionRect;
+	public GUIStyle style;
+	// Use this for initialization
+	void Start () {
+		characters = new List<GameObject>();
+		characters.AddRange(GameObject.FindGameObjectsWithTag("Character"));
+		Debug.Log(characters.Count);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			mouseStartPosition = Input.mousePosition;
+			mouseDown = true;
+			
+			//To do
+			//ClearSelect()
+		}
+		if(Input.GetMouseButtonUp(0)){
+			mouseDown = false;
+			
+		}
+		
+		if(mouseDown){
+			selectionRect.xMin = mouseStartPosition.x < Input.mousePosition.x? mouseStartPosition.x: Input.mousePosition.x;
+			selectionRect.xMax = mouseStartPosition.x > Input.mousePosition.x? mouseStartPosition.x: Input.mousePosition.x;
+			selectionRect.yMin = Camera.main.pixelHeight - (mouseStartPosition.y > Input.mousePosition.y? mouseStartPosition.y: Input.mousePosition.y);
+			selectionRect.yMax = Camera.main.pixelHeight - (mouseStartPosition.y < Input.mousePosition.y? mouseStartPosition.y: Input.mousePosition.y);
+			
+			characters.RemoveAll(item => item == null);
+			foreach(GameObject c in characters){
+				Vector3 screenPos = Camera.main.WorldToScreenPoint(c.transform.position);
+				screenPos.y = Camera.main.pixelHeight -screenPos.y;
+				//print (""+screenPos+" - "+Input.mousePosition);
+				if(selectionRect.Contains(screenPos)){
+					Debug.Log("!");
+					c.animation.CrossFade("Guard_Dying");
+					//To do
+					//Select()
+				}
+			}
+		}
+	}
+	
+	void OnGUI () {
+		// Make a background box
+		if(mouseDown)GUI.Box(selectionRect, "");
+		foreach(GameObject c in characters){
+			Vector3 screenPos = Camera.main.WorldToScreenPoint(c.transform.position);
+			GUI.Box(new Rect(screenPos.x,Camera.main.pixelHeight -screenPos.y,1,1),"");
+		}
+	}
+}
---
+++ Feuds/Assets/Scripts/UI/CameraMove.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+	private const int SCROLL_THRESH = 100;
+	private const float SCROLL_SPEED = 1f;
+	private const float MAX_SPEED = .5f;
+	private const float BOTTOM_MARGIN = 192;
+	private float TIME_SCROLL = 0;
+
+	public float zoomSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mouse_pos = Input.mousePosition;
+
+		float height = Screen.height-BOTTOM_MARGIN;
+
+		float x_off = 0;
+		float z_off = 0;
+
+		if(mouse_pos.y < SCROLL_THRESH + BOTTOM_MARGIN && mouse_pos.y >= BOTTOM_MARGIN){
+			x_off += SCROLL_SPEED;
+			z_off += -SCROLL_SPEED;
+		}
+		else if(mouse_pos.y > Screen.height - SCROLL_THRESH){
+			x_off += -SCROLL_SPEED;
+			z_off += SCROLL_SPEED;
+		}
+
+		if(mouse_pos.x < SCROLL_THRESH){
+			x_off += -SCROLL_SPEED;
+			z_off += -SCROLL_SPEED;
+		}
+		else if(mouse_pos.x > Screen.width - SCROLL_THRESH){
+			x_off += SCROLL_SPEED;
+			z_off += SCROLL_SPEED;
+		}
+
+		if(x_off == 0 && z_off == 0){
+			TIME_SCROLL = 0;
+		}
+		else
+			TIME_SCROLL += Time.deltaTime;
+
+		if(mouse_pos.y < BOTTOM_MARGIN){
+			x_off = 0;
+			z_off = 0;
+		}
+
+		x_off *= TIME_SCROLL;
+		z_off *= TIME_SCROLL;
+
+		if(Mathf.Abs (x_off) > MAX_SPEED)
+			x_off = (x_off / Mathf.Abs (x_off)) * MAX_SPEED;
+		if(Mathf.Abs (z_off) > MAX_SPEED)
+			z_off = (z_off / Mathf.Abs (z_off)) * MAX_SPEED;
+
+		this.transform.position = this.transform.position + new Vector3(x_off, 0, z_off);
+		
+		float cam_size = Camera.main.orthographicSize-Input.GetAxis("Mouse ScrollWheel")*zoomSpeed;
+
+		if (cam_size > 30)
+			cam_size = 30;
+		else if (cam_size < 5)
+			cam_size = 5;
+
+		Camera.main.orthographicSize = cam_size;
+	}
+}
