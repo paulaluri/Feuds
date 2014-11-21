cbc7404ef1f0b81b6a048012fc86b73032acac1e
Jacob M. Slone <jslone1993@gmail.com>
2014-11-20 22:20:24 -0500

    changed GetTarget, now uses the ActionController's cached position so defense doesn't overextend

--- Feuds/Assets/Scripts/AI/Actions/GetTarget.cs
+++ Feuds/Assets/Scripts/AI/Actions/GetTarget.cs
@@ -5,7 +5,7 @@ public class GetTarget : Action {
 
 	// Update is called once per frame
 	public override bool Update () {
-		Collider[] enemies = Physics.OverlapSphere(ac.transform.position,30.0f,ac.attackables);
+		Collider[] enemies = Physics.OverlapSphere(ac.position,30.0f,ac.attackables);
 		if(enemies.Length > 0) {
 			float d = float.MaxValue;
 			GameObject closest = null;
