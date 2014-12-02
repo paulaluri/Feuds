5bb4425703023af466ffa57a4b2deafb94916b23
paulaluri <paulaluri@Pauls-MacBook-Pro.local>
2014-12-02 14:09:32 -0500

    Cleaned up magicbullet

--- Feuds/Assets/Scripts/MagicBullet.cs
+++ Feuds/Assets/Scripts/MagicBullet.cs
@@ -3,11 +3,9 @@ using System.Collections;
 
 public class MagicBullet : MonoBehaviour {
 	private bool isFiring = false;
-	private float initTime = 0;
 	public float speed = 1f;
 	// Use this for initialization
 	void Start () {
-		initTime = Time.realtimeSinceStartup;
 		ParticleSystem p = this.GetComponentInChildren<ParticleSystem>();
 		if(p){
 			p.Simulate (10);
