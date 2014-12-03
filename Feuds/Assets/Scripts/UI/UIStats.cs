875dddf64aab0e450e0a9c7bf83ab9646cbdfdb5
paulaluri <paul@aluri.org>
2014-12-02 21:20:43 -0500

    Visual but NOT functional stats screen

---
+++ Feuds/Assets/Scripts/UI/UIStats.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIStats : MonoBehaviour {
+	public Texture check;
+	public Texture p1Track;
+	public Texture p1End;
+	public Texture p2Track;
+	public Texture p2End;
+	public GUIStyle menu_btn;
+	public GUIStyle text_style;
+
+	private const int BAR_HEIGHT = 64;
+	private const int END_WIDTH = 32;
+
+	// Use this for initialization
+	void Start () {
+		p2Track.wrapMode = TextureWrapMode.Repeat;
+		p1Track.wrapMode = TextureWrapMode.Repeat;
+		p2End.wrapMode = TextureWrapMode.Repeat;
+		p1End.wrapMode = TextureWrapMode.Repeat;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		//Continue OR MAIN MENU if game done
+		if(GUI.Button(new Rect(Screen.width-182, Screen.height-58, 172, 48), "Continue", menu_btn)) {
+
+		}
+
+		//Feud Balance bar
+		DrawBar(0.5f);
+
+		//Feud Rounds
+		GUI.BeginGroup(new Rect(Screen.width/2 - 200, 180, 400, 400), text_style);
+
+		DrawText(new Rect(0, 0, 100, 60), "Round", text_style, true, 30, Color.white);
+		DrawText(new Rect(100, 0, 150, 60), "Player 1", text_style, true, 30, Color.white);
+		DrawText(new Rect(250, 0, 150, 60), "Player 2", text_style, true, 30, Color.white);
+
+		int latest = 2;
+		for(int i = 0; i < 6; i++){
+			DrawText(new Rect(0, (i+1)*50, 100, 60), (i+1).ToString(), text_style, true, 30, latest==i?(new Color(255f/255f, 216f/255f, 0)):Color.white);
+			GUI.DrawTexture(new Rect(150,(i+1)*50, 50, 50), check);
+		}
+
+		GUI.EndGroup();
+
+		//Feud Title Bar
+		DrawText(new Rect(0, 10, Screen.width, 70), "Victory", text_style, true, 50, Color.white);
+	}
+
+	void DrawText(Rect r, string s, GUIStyle g, bool outline, int size, Color c){
+		GUIStyle cp = new GUIStyle(g);
+		cp.fontSize = size;
+		cp.normal.textColor = c;
+
+		if(outline){
+			Rect br = new Rect(r.x-1, r.y-1, r.width, r.height);
+			Rect ur = new Rect(r.x-1, r.y+1, r.width, r.height);
+			Rect bl = new Rect(r.x+1, r.y-1, r.width, r.height);
+			Rect ul = new Rect(r.x+1, r.y+1, r.width, r.height);
+
+			GUIStyle shadow = new GUIStyle(cp);
+			shadow.normal.textColor = Color.black;
+
+			GUI.Label (br, s, shadow);
+			GUI.Label (ur, s, shadow);
+			GUI.Label (bl, s, shadow);
+			GUI.Label (ul, s, shadow);
+		}
+
+		GUI.Label (r, s, cp);
+	}
+
+	void DrawBar(float val){
+		float bar_width =  Screen.width - 100;
+		float width_to_draw = val*bar_width;
+		float final_width = width_to_draw;
+
+		GUI.BeginGroup(new Rect(50, 100, bar_width, BAR_HEIGHT));
+
+		//Draw Bottom
+		GUI.DrawTexture (new Rect(0, 0, END_WIDTH*2, BAR_HEIGHT), p2End);
+		GUI.DrawTexture (new Rect(bar_width-END_WIDTH*2, 0, END_WIDTH*2, BAR_HEIGHT), p2End);
+		GUI.DrawTextureWithTexCoords(new Rect(END_WIDTH, 0, bar_width-END_WIDTH*2, BAR_HEIGHT), p2Track, new Rect(0, 0, bar_width / p1Track.width, 1));
+
+		//Draw Top
+		if(width_to_draw > 0){
+			if(width_to_draw > END_WIDTH)
+				GUI.DrawTextureWithTexCoords(new Rect(0, 0, END_WIDTH, BAR_HEIGHT), p1End, new Rect(0, 0, .5f, 1));
+			else
+				GUI.DrawTextureWithTexCoords(new Rect(0, 0, width_to_draw, BAR_HEIGHT), p1End, new Rect(0, 0, width_to_draw/END_WIDTH*2, 1));
+			width_to_draw -= END_WIDTH;
+		}
+
+		if(END_WIDTH > (bar_width-final_width)){
+			GUI.DrawTextureWithTexCoords(new Rect(bar_width-END_WIDTH*2, 0, END_WIDTH*2-(bar_width-final_width), BAR_HEIGHT), p1End, new Rect(0, 0, (END_WIDTH*2-(bar_width-final_width))/END_WIDTH*2, 1));
+		}
+
+		if(width_to_draw > 0){
+			float track_width = Mathf.Min (width_to_draw, bar_width - END_WIDTH*2);
+			GUI.DrawTextureWithTexCoords(new Rect(END_WIDTH, 0, track_width, BAR_HEIGHT), p1Track, new Rect(0, 0, .1f, 1));
+
+			width_to_draw -= track_width;
+		}
+
+		GUI.EndGroup ();
+	}
+}
