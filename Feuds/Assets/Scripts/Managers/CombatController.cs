b04e2ef16635f69e96155be0f291752a4a82bd98
paulaluri <paulaluri@Pauls-MBP.wv.cc.cmu.edu>
2014-12-03 20:26:25 -0500

    Boost icons

--- Feuds/Assets/Scripts/Managers/CombatController.cs
+++ Feuds/Assets/Scripts/Managers/CombatController.cs
@@ -56,6 +56,10 @@ public class CombatController : MonoBehaviour {
 	public bool inSkill = false;
 	public bool attackedThisFrame = false;
 
+	public bool hasAttackBoost = false;
+	public bool hasDefenseBoost = false;
+	public bool hasResistBoost = false;
+
 	private NavMeshAgent agent;
 
 	// Use this for initialization
@@ -130,6 +134,11 @@ public class CombatController : MonoBehaviour {
 		Attack *= atkScale;
 		Defense.physical += physicalDef;
 		Defense.magic += magicDef;
+
+		hasAttackBoost = atkScale > 1;
+		hasDefenseBoost = physicalDef > 0;
+		hasResistBoost = magicDef > 0;
+
 		if(broadcast) {
 			networkView.RPC("SetBonus",RPCMode.OthersBuffered,atkScale,physicalDef,magicDef,false);
 		}
--- Feuds/Assets/Scripts/MenuNavigation/nav_MAIN.cs
+++ Feuds/Assets/Scripts/MenuNavigation/nav_MAIN.cs
@@ -16,10 +16,14 @@ public class nav_MAIN : MonoBehaviour {
 		GUI.DrawTexture (new Rect(Screen.width/2 - 125, 100, 250, 100), logo);
 
 		//Options
-		GUI.Button(new Rect(Screen.width/2 - 86, 250, 172, 48), "Single Player", menu_btn);
-		if(GUI.Button(new Rect(Screen.width/2 - 86, 298, 172, 48), "Multiplayer", menu_btn))
+		//GUI.Button(new Rect(Screen.width/2 - 86, 250, 172, 48), "Single Player", menu_btn);
+		GUI.BeginGroup (new Rect(0, Screen.height / 2 - (2 * 48), Screen.width, 96)); 
+
+		if(GUI.Button(new Rect(Screen.width/2 - 86, 0, 172, 48), "Multiplayer", menu_btn))
 			Application.LoadLevel(SceneLobby);
-		GUI.Button(new Rect(Screen.width/2 - 86, 346, 172, 48), "About", menu_btn);
+		GUI.Button(new Rect(Screen.width/2 - 86, 48, 172, 48), "About", menu_btn);
+
+		GUI.EndGroup ();
 
 		//Credits
 		GUI.Label (new Rect(10, Screen.height-50,Screen.width-20, 30), "Created by Paul Aluri, Pasan Julsaksrisakul, and Jacob Slone", credit_style);
