c3823a4e45126e12d65ab05de5d58179e02a7ea7
paulaluri <paulaluri@Pauls-MBP.wv.cc.cmu.edu>
2014-12-03 19:03:08 -0500

    Some updates

--- Feuds/Assets/Scripts/MagicBullet.cs
+++ Feuds/Assets/Scripts/MagicBullet.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class MagicBullet : MonoBehaviour {
 	public float speed = 1f;
+	private GameObject shooter;
+	private Vector3 origin = Vector3.zero;
 	// Use this for initialization
 	void Start () {
 		ParticleSystem p = this.GetComponentInChildren<ParticleSystem>();
@@ -15,5 +17,12 @@ public class MagicBullet : MonoBehaviour {
 	public void Fire (Transform init, Vector3 t) {
 		this.transform.forward = (t - init.position).normalized;
 		rigidbody.velocity = this.transform.forward * 10f;
+		origin = init.position;
+		shooter = init.gameObject;
+	}
+
+	void Update(){
+		if (Vector3.Distance (this.transform.position, origin) > shooter.GetComponent<CombatController> ().Radius)
+			GameObject.Destroy (this.gameObject);
 	}
 }
--- Feuds/Assets/Scripts/UI/UICharacter.cs
+++ Feuds/Assets/Scripts/UI/UICharacter.cs
@@ -36,7 +36,8 @@ public class UICharacter : MonoBehaviour {
 		if (!combat.isDead) {
             if (rendering)
             {
-                Vector3 screen = Camera.main.WorldToScreenPoint(transform.position + new Vector3(0, 2.2f, 0));
+				if(Camera.main)
+                	Vector3 screen = Camera.main.WorldToScreenPoint(transform.position + new Vector3(0, 2.2f, 0));
 
                 GUI.DrawTexture(new Rect(screen.x - 32, Screen.height - screen.y, 64, 4), Wound);
                 GUI.DrawTexture(new Rect(screen.x - 32, Screen.height - screen.y, Mathf.Max(1, (combat.Health.current / combat.Health.max) * 64), 4), Health);
